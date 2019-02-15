1dc30d18e7acc28eb399a14ec5550122b71113dd
umi2019 <uamanuma@gmail.com>
2019-02-15 18:42:12 +0900

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_053536) do
+ActiveRecord::Schema.define(version: 2019_02_15_085316) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
