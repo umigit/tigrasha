d4edca57ef2b8fd388c21483ce94af87d06d1893
umi2019 <uamanuma@gmail.com>
2019-02-15 19:15:36 +0900

    Make view for #new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,17 @@ class PostsController < ApplicationController
   def index
     @post = Post.find(1)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+
+  def post_params
+  end
 end
