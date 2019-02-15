6a633699f3263ce8d7c9bd6a78e0621fa51ff24e
umi2019 <uamanuma@gmail.com>
2019-02-15 14:28:15 +0900

    Devise install

--- config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
-end
---
+++ db/migrate/20190215051437_devise_create_users.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+
+  end
+end
