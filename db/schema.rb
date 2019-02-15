42b29cb32490747dc8bb2bfe7008f1a88a492b19
umi2019 <uamanuma@gmail.com>
2019-02-15 12:24:20 +0900

    Install jQuery and Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_013022) do
+ActiveRecord::Schema.define(version: 2019_02_15_031755) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_013022) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
