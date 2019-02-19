7bd7942756e935d89e89f034cc091687e05f6809
umi2019 <uamanuma@gmail.com>
2019-02-19 11:46:54 +0900

    Add user_id to sightings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_010742) do
+ActiveRecord::Schema.define(version: 2019_02_19_023050) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -53,7 +53,9 @@ ActiveRecord::Schema.define(version: 2019_02_19_010742) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_sightings_on_post_id"
+    t.index ["user_id"], name: "index_sightings_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
