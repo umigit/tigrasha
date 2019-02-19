b497c86c225707101b4b0f5b8b92c54ee798ee8f
umi2019 <uamanuma@gmail.com>
2019-02-19 19:54:19 +0900

    Edit sightings controller

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,22 +1,27 @@
 class SightingsController < ApplicationController
   before_action :set_sighting
   def index
-    @sithtings = Sighting.all
+    if params[:sighting].present?
+      @sightings = Sighting.ransack(place_or_detail_matches: "%#{params[:sighting][:keyword]}%").result
+    end
   end
 
   def new
-    @sighting = Sighting.new
   end
 
   def create
     @sighting = Sighting.new(sighting_params)
-    @sighting.save
+    if @sighting.save
+      redirect_to sightings_path
+    else
+      render :new
+    end
   end
 
   private
 
   def sighting_params
-    params.require(:sighting).permit(:plece, :date, :image).merge(user_id: current_user.id)
+    params.require(:sighting).permit(:detail, :image, :place, :date).merge(user_id: current_user.id)
   end
 
   def set_sighting
