5fe92dcd190a6cd7464f70418dff22a5b8165cc5
umi2019 <uamanuma@gmail.com>
2019-02-19 11:51:29 +0900

    Add shightings#create method

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -3,7 +3,16 @@ class SightingsController < ApplicationController
     @sighting = Sighting.new
   end
 
+  def create
+    @sighting = Sighting.new(sighting_params)
+    @sighting.save
+  end
+
   private
+
+  def sighting_params
+    params.require(:sighting).permit(:plece, :date, :image).merge(user_id: current_user.id)
+  end
 
   def set_sighting
   end
