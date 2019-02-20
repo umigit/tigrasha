8ca13e3f2293d5650c909f7e4c6348dd1803be2d
umi2019 <uamanuma@gmail.com>
2019-02-20 12:38:28 +0900

    Add ShowGoogleMap function

--- app/assets/javascripts/tigrasha.js
+++ app/assets/javascripts/tigrasha.js
@@ -1,4 +1,25 @@
 $(function(){
+  function ShowGoogleMap(latitude, longitude){
+    handler = Gmaps.build('Google');
+    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+      markers = handler.addMarkers([
+        {
+          "lat": latitude,
+          "lng": longitude,
+          "picture": {
+            "url": "http://people.mozilla.com/~faaborg/files/shiretoko/firefoxIcon/firefox-32.png",
+            "width":  32,
+            "height": 32
+          },
+          "infowindow": "目撃場所"
+        }
+      ]);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+    });
+  }
+
+
   $("#imageFile").on('change', function(event) {
     event.preventDefault();
 
