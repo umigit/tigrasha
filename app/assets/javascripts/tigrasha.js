18937cf82873cfe69debd2d5becbea7d5eca5388
umi2019 <uamanuma@gmail.com>
2019-02-20 16:35:23 +0900

    Edit js

--- app/assets/javascripts/tigrasha.js
+++ app/assets/javascripts/tigrasha.js
@@ -1,27 +1,4 @@
 $(function(){
-  function ShowGoogleMap(latitude, longitude){
-    handler = Gmaps.build('Google');
-    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-
-      markers = handler.addMarkers([
-        {
-          "lat": latitude,
-          "lng": longitude,
-          "infowindow": "目撃場所",
-          // "picture": {
-          //   "url": URL.createObjectURL($("#imageFile")[0].files[0]),
-          //   "width":  200,
-          //   "height": 200
-          // },
-        }
-      ]);
-      handler.bounds.extendWith(markers);
-      handler.fitMapToBounds();
-      handler.getMap().setZoom(15);
-    });
-  }
-
-
   $("#imageFile").on('change', function(event) {
     event.preventDefault();
 
@@ -35,8 +12,43 @@ $(function(){
         var longitude = longitudeArray[0] + longitudeArray[1] / 60 + longitudeArray[2] / 3600;
         $("#map").height($("#map").width());
         ShowGoogleMap(latitude, longitude);
-        $("#addressField").val(latitude.toString() + "," + longitude.toString());
+        $("#locationField").val(latitude.toString() + "," + longitude.toString());
       }
     });
   });
 });
+
+$(document).on('turbolinks:load', function(){
+  var point = [];
+  if ($("#map").data("location")){
+    point = $("#map").data("location").split(",");
+  }
+
+  if (point.length == 2){
+    $("#map").height($("#map").width());
+    ShowGoogleMap(point[0], point[1]);
+    console.log(point[0]);
+  }
+});
+
+function ShowGoogleMap(latitude, longitude){
+  handler = Gmaps.build('Google');
+  handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
+
+    markers = handler.addMarkers([
+      {
+        "lat": latitude,
+        "lng": longitude,
+        // "infowindow": "目撃場所",
+        // "picture": {
+        //   "url": URL.createObjectURL($("#imageFile")[0].files[0]),
+        //   "width":  200,
+        //   "height": 200
+        // },
+      }
+    ]);
+    handler.bounds.extendWith(markers);
+    handler.fitMapToBounds();
+    handler.getMap().setZoom(15);
+  });
+}
