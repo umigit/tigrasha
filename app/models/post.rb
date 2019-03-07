9797993af656a9ad7fe344f090241cda8115fc92
umi2019 <uamanuma@gmail.com>
2019-03-07 13:06:28 +0900

    Carrier wave discription

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  has_one_attached :image
+  # has_one_attached :image
   belongs_to :user
-
+  mount_uploader :image, ImageUploader
   validates :title, presence: true
   validates :name, presence: true
   validates :lost_place, presence: true
