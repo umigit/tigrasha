f633ef6094253036cc0e11f9e512386e4cbb5cb8
umigit <hamu56Kiki>
2019-04-09 01:15:53 +0900

    migration

--- db/migrate/20190221095835_add_latlng_to_posts_and_sightings.rb
+++ db/migrate/20190221095835_add_latlng_to_posts_and_sightings.rb
@@ -1,5 +1,15 @@
 class AddLatlngToPostsAndSightings < ActiveRecord::Migration[5.2]
   def change
+    create_table :posts do |t|
+      t.string :title, null: false, default: ""
+      t.string :name, null: false, default: ""
+      t.string :lost_plase, null: false, default: ""
+      t.datetime :lost_date, null: false, default: -> {'NOW()'}
+      t.string :address
+      t.text :detail
+      t.timestamps
+    end
+
     add_column :posts, :latitude, :float
     add_column :posts, :longitude, :float
     add_column :sightings, :latitude, :float
