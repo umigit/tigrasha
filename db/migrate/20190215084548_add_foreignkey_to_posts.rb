8a244ba5c596d9f96f945019ef6d2703fc29148b
umigit <hamu56Kiki>
2019-04-09 01:16:10 +0900

    migration

--- db/migrate/20190215084548_add_foreignkey_to_posts.rb
+++ db/migrate/20190215084548_add_foreignkey_to_posts.rb
@@ -1,14 +1,6 @@
 class AddForeignkeyToPosts < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
-      t.string :title, null: false, default: ""
-      t.string :name, null: false, default: ""
-      t.string :lost_plase, null: false, default: ""
-      t.datetime :lost_date, null: false, default: -> {'NOW()'}
-      t.string :address
-      t.text :detail
-      t.timestamps
-    end
+
   end
 end
 
