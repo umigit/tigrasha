eb5d582755385416ebd4b511f9424b7d3df9fab4
umigit <hamu56Kiki>
2019-04-09 00:45:20 +0900

    edit migration

--- db/migrate/20190221095243_remove_location_from_postss_and_sightings.rb
+++ db/migrate/20190221095243_remove_location_from_postss_and_sightings.rb
@@ -1,6 +1,6 @@
 class RemoveLocationFromPostssAndSightings < ActiveRecord::Migration[5.2]
   def change
-    remove_column :posts, :location
+    # remove_column :posts, :location
     remove_column :sightings, :location
   end
 end
