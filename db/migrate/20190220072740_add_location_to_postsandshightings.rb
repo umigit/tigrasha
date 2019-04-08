d367ede67fc510a2b5e79b96ea049e823bd8f791
umigit <hamu56Kiki>
2019-04-09 00:44:17 +0900

    edit migration

--- db/migrate/20190220072740_add_location_to_postsandshightings.rb
+++ db/migrate/20190220072740_add_location_to_postsandshightings.rb
@@ -1,6 +1,6 @@
 class AddLocationToPostsandshightings < ActiveRecord::Migration[5.2]
   def change
-    add_column :posts, :location, :string
+    # add_column :posts, :location, :string
     add_column :sightings, :location, :string
   end
 end
